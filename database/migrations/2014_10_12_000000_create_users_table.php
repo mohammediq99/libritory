b9570288e5dd0917881e79580efd6ae8ce3f3316
mohammediq99 <30753973+mohammediq99@users.noreply.github.com>
2021-01-09 19:00:28 +0300

    Update 2014_10_12_000000_create_users_table.php

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -18,7 +18,7 @@ class CreateUsersTable extends Migration
             $table->string('name');
             $table->string('username')->unique();
             $table->string('type')->default('user');
-            $table->string('email')->unique();
+            $table->string('email')->unique()->nullable();
             $table->string('age',3)->nullable();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
